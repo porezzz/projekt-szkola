ba4da326050416d8522e0430aa3a099ca4fc96f6
porezzz <jakub.marsz.04@gmail.com>
2023-11-20 17:20:22 +0100

    hamb working

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
-const nav = document.querySelectorAll("nav")[0];
-console.log(nav.classList);
-
-const hamb = () => {
-  this.addEventListener("click", () => {
-    nav.classList = "block" ? "none" : 'block';
-    console.log(nav.classList);
-  });
-};
+const nav = document.querySelector('nav')
+const hambToggle = () => {
+  const hamb = document.querySelector('.menu')
+  hamb.addEventListener('click', () => {
+    if(hamb.textContent === "menu"){
+      hamb.textContent = "close"
+      nav.style.display = "flex"
+    } else {
+      hamb.textContent = "menu"
+      nav.style.display = "none"
+    }
+  })
+  const ul = document.querySelectorAll('li')
+  ul.forEach(ele => {
+    ele.addEventListener('click', () => {
+      hamb.textContent = "menu"
+      nav.style.display = "none"
+    })
+  })
+  
+}; hambToggle();
