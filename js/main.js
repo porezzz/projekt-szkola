eda1569873c33b3cefb55ec0014a0e9b75090c0d
porezzz <jakub.marsz.04@gmail.com>
2023-11-22 17:34:26 +0100

    semi working hamburger animation

--- js/main.js
+++ js/main.js
@@ -1,21 +1,39 @@
-const nav = document.querySelector('nav')
+const nav = document.querySelector("nav");
+const Anim = (hamb, nav, state) => {
+  if (state === "Out") {
+    hamb.style.display = "none";
+    nav.classList.add("animNavRev");
+    setTimeout(() => {
+      nav.style.top = "-1000px";
+      nav.classList.remove("animNavRev");
+      hamb.textContent = "menu";
+      hamb.style.display = "block";
+    }, 450);
+  } else if (state === "In") {
+    hamb.style.display = "none";
+    nav.classList.add("animNav");
+    setTimeout(() => {
+      nav.style.top = "0px";
+      nav.classList.remove("animNav");
+      hamb.textContent = "close";
+      hamb.style.display = "block";
+    }, 450);
+  }
+};
 const hambToggle = () => {
-  const hamb = document.querySelector('.menu')
-  hamb.addEventListener('click', () => {
-    if(hamb.textContent === "menu"){
-      hamb.textContent = "close"
-      nav.style.display = "flex"
+  const hamb = document.querySelector(".menu");
+  hamb.addEventListener("click", () => {
+    if (hamb.textContent === "menu") {
+      Anim(hamb, nav, "In");
     } else {
-      hamb.textContent = "menu"
-      nav.style.display = "none"
+      Anim(hamb, nav, "Out");
     }
-  })
-  const ul = document.querySelectorAll('li')
-  ul.forEach(ele => {
-    ele.addEventListener('click', () => {
-      hamb.textContent = "menu"
-      nav.style.display = "none"
-    })
-  })
-  
-}; hambToggle();
+  });
+  const ul = document.querySelectorAll("li");
+  ul.forEach((ele) => {
+    ele.addEventListener("click", () => {
+      Anim(hamb, nav, "Out");
+    });
+  });
+};
+hambToggle();
